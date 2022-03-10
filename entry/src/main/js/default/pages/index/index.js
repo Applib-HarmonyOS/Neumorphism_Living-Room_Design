2c74bc949c7e465c3d705a745a69627e5b8ab753
Aryan Gupta <arynn.gupta@gmail.com>
2022-03-10 12:37:53 +0530

    update build.gradle

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,3 +1,5 @@
+import fetch from '@system.fetch';
+
 export default {
     data: {
         username: "Username",
@@ -12,11 +14,21 @@ export default {
         washing_machine: false,
         refrigerator: false,
         carousel: true,
-        temperature: 18,
         dry_mode: false,
         cool_mode: false,
         fan_mode: false,
         turbo_mode: false,
+        lamp1_p: "0",
+        lamp2_p: "0",
+        fan1_p: "0",
+        fan2_p: "0",
+        temperature: 0,
+        humidity: 0,
+        location: "Bangalore",
+        weather: "Rainy",
+        day: "Mob, Jan 01",
+        alert_title: "Title",
+        alert_desc: "Description"
     },
     toggleRoom(room){
         for(var i=0;i<5;i++){
@@ -127,4 +139,53 @@ export default {
     onWeekClick: function() {
         console.log("Week Button clicked !");
     },
+    fetchData : function(){
+        let data;
+        fetch.fetch({
+            url:'https://neumorphism-api.herokuapp.com/neumorphism/living_room',
+            responseType:"json",
+            method: 'GET',
+            success:function(resp)
+            {
+                data = JSON.parse(resp.data);
+            },
+            fail:(data,code) => {
+                console.log("fail data:"+ JSON.stringify(data));
+                console.log("fail code:"+ code)
+            },
+            complete: ()=>{
+                this.username = data.username;
+                this.living_room_active = data.living_room_active;
+                this.dining_room_active = data.dining_room_active;
+                this.bed_room_active = data.bed_room_active;
+                this.bath_room_active = data.bath_room_active;
+                this.backyard_room_active = data.backyard_room_active;
+                this.living_room = data.living_room;
+                this.smart_tv = data.smart_tv;
+                this.ac = data.ac;
+                this.washing_machine = data.washing_machine;
+                this.refrigerator = data.refrigerator;
+                this.carousel = data.carousel;
+                this.dry_mode = data.dry_mode;
+                this.cool_mode = data.cool_mode;
+                this.fan_mode = data.fan_mode;
+                this.turbo_mode = data.turbo_mode;
+                this.lamp1_p = data.lamp1_p;
+                this.lamp2_p = data.lamp2_p;
+                this.fan1_p = data.fan1_p;
+                this.fan2_p = data.fan2_p;
+                this.temperature = data.temperature;
+                this.humidity = data.humidity;
+                this.location = data.location;
+                this.weather = data.weather;
+                this.day = data.day;
+                this.alert_title = data.alert_title;
+                this.alert_desc = data.alert_desc;
+            }
+        })
+    },
+    onInit(){
+        this.fetchData();
+        setInterval(this.fetchDateAndTime, 1000);
+    }
 }
